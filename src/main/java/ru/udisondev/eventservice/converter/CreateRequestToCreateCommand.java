475747efe25678cabd963f03e7f66f1dd0eef033
Sergei Kanaev <sergei.kanaev@tabbank.com>
2021-01-07 13:02:09 +0400

    fix converters

--- src/main/java/ru/udisondev/eventservice/converter/CreateRequestToCreateCommand.java
+++ src/main/java/ru/udisondev/eventservice/converter/CreateRequestToCreateCommand.java
@@ -10,32 +10,17 @@ public class CreateRequestToCreateCommand implements Converter<CreateRequest, Cr
     @Override
     public CreateCommand convert(CreateRequest source) {
         Assert.notNull(source, "CreateRequest must not be null");
-        checkSource(source);
-        return new CreateCommand(
-                source.getCustomerId(),
-                source.getTitle(),
-                source.getTypeId(),
-                source.getCity(),
-                source.getDescription(),
-                source.getPlace(),
-                source.getStartTs(),
-                source.getEndTs()
-        );
+
+        return CreateCommand.newCreateCommand()
+                .withTitle(source.getTitle())
+                .withDescription(source.getDescription())
+                .withCustomerId(source.getCustomerId())
+                .withTypeId(source.getTypeId())
+                .withCity(source.getCity())
+                .withPlace(source.getPlace())
+                .withStartTs(source.getStartTs())
+                .withEndTs(source.getEndTs())
+                .build();
     }
 
-    private void checkSource(CreateRequest source) {
-        if (source.getTitle() == null) throw new IllegalArgumentException("title must not be null");
-        if (source.getDescription() == null) throw new IllegalArgumentException("description must not be null");
-        if (source.getId() == null) throw new IllegalArgumentException("id must not be null");
-        if (source.getCity() == null) throw new IllegalArgumentException("city must not be null");
-        if (source.getCustomerId() == null) throw new IllegalArgumentException("customerId must not be null");
-        if (source.getTypeId() == null) throw new IllegalArgumentException("typeId must not be null");
-        if (source.getTitle().length() < 2) throw new IllegalArgumentException("title's length must be greater than 2 characters");
-        if (source.getTitle().length() > 64) throw new IllegalArgumentException("title's length must be less than 64 characters");
-        if (source.getDescription().length() < 20) throw new IllegalArgumentException("description's length must be greater than 20 characters");
-        if (source.getDescription().length() > 10000) throw new IllegalArgumentException("description's length must be less than 10000 characters");
-        if (source.getCity().isBlank()) throw new IllegalArgumentException("city must not be blank");
-        if (source.getCity().length() < 2) throw new IllegalArgumentException("city's length must be greater than 2 characters");
-        if (source.getCity().length() > 64) throw new IllegalArgumentException("city's length must be less than 64 characters");
-    }
 }
